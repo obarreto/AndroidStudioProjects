f31d1112f8dbeb2022c3f1de46a90d9bc96a691e
obarreto <ozbarreto@outlook.com>
2020-02-21 21:18:30 -0300

    Aulas de android Coursera UNICAMP 21/02

--- AppSoma/app/src/main/java/com/example/appsoma/MainActivity.java
+++ AppSoma/app/src/main/java/com/example/appsoma/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 //AppCompat utiliza retrocompatividade com outras activity e necessariamente irá herda da AppComptActivity
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     public EditText txtNum1;
     public EditText txtNum2;
@@ -15,17 +16,49 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    //Um metodo do ciclo de vida da Activity.
+    //O metodo onCreate é o primeiro ser chamado pelo AndroidStudio, e é utilizado como ponto de partida
     protected void onCreate(Bundle savedInstanceState) {
+
+        /*
+        1 - super.onCreate avisa ao sistema Android que além do codigo que vamos inserir aqui , queremos
+        que o código que está no método onCreate da classe pai seja executado. Caso não insira essa
+        chamada a exceção "Super not called exception" será dispara.
+        */
         super.onCreate(savedInstanceState);
+
+        /*
+        2 - Essa chamada recebe como parâmetro a id do código XML que servirá de interface gráfica
+        para esta activity. Em set.ContentView, podemos acessar os elementos que estão que estão no
+        arquivo XML precedido por R.layout
+        */
         setContentView(R.layout.activity_main);
-        //R.id acessa o rsoource
+
+        /*
+        3 - findViewById irá criar uma referência para o elemento da interface gráfica e eu vou
+        adicionar essa referência como atributo da classe. Porém deve-se garantir que um ID foi foi
+        configurado no código XML na marcação da view, como está abaixo - txtNum1, txtNum2, ldlResultado
+        foram configurado em app -> res(resource) -> layout -> activity_main.xml. Como o metodo
+        findViewById retorna o objeto da classe View é necessário sempre converter esse objeto para
+        o tipo de dado específico conrrespondete. R.id acessa o rsource
+        */
+
+        textView = (TextView)findViewById(R.id.textView);
+
+        System.out.println(textView.getText());
         this.txtNum1 = findViewById(R.id.txtNum1);
         this.txtNum2 = findViewById(R.id.txtNum2);
         this.ldlResultado = findViewById(R.id.ldlResultado);
     }
-        public void somar(View view){
+   /* float number1 = 6;
+    float number2 = 4;
 
+    //Instaciando o metodo Operacoes
+    Operacoes operacoes = new Operacoes(number1, number2);
+		System.out.println(operacoes.soma());
+		System.out.println(operacoes.subtracao());
+		System.out.println(operacoes.multiplicacao());
+		System.out.println(operacoes.divisao());*/
+        public void somar(View view){
 
         int num1 = Integer.parseInt(txtNum1.getText().toString());
         int num2 = Integer.parseInt(txtNum2.getText().toString());
--- AppSoma/app/src/main/java/com/example/appsoma/Operacoes.java
+++ AppSoma/app/src/main/java/com/example/appsoma/Operacoes.java
@@ -1,4 +1,30 @@
 package com.example.appsoma;
 
 public class Operacoes {
+
+    private float a;
+    private float b;
+
+    //Metodo construtor Operacao
+    public Operacoes(float a, float b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public float soma(){
+        return a + b;
+    }
+
+    public float subtracao(){
+        return a - b;
+    }
+
+    public float multiplicacao(){
+        return a * b;
+    }
+
+    public float divisao(){
+        return a / b;
+    }
 }
+
